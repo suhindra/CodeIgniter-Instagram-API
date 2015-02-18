ec1e36a6187ac72c83c75b8554dee11478dbfa15
Suhindra <suhindra@hotmail.co.id>
2015-02-18 16:57:53 +0700

    Update instagram_api.php

--- Demo/application/config/instagram_api.php
+++ Demo/application/config/instagram_api.php
@@ -29,5 +29,5 @@ $config['instagram_scope'] = '';
 
 // There was issues with some servers not being able to retrieve the data through https
 // If you have this problem set the following to FALSE 
-// See https://github.com/ianckc/CodeIgniter-Instagram-Library/issues/5 for a discussion on this
+
 $config['instagram_ssl_verify']		= TRUE;
